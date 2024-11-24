bca165d3cec6ad89b17aae4291d1fe0f8bfffa2f
matthewthelam@gmail.com <matthewthelam@gmail.com>
2024-11-23 19:47:46 -0800

    Initial Commit

---
+++ LearnLanguagebyGivingThanks/LearnLanguagebyGivingThanksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLanguagebyGivingThanksApp.swift
+//  LearnLanguagebyGivingThanks
+//
+//  Created by Matt Lam on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnLanguagebyGivingThanksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
